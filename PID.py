4536b4e351ec01bf2169659d41f776587001c96b
Junyi Lou <junyi_lou@outlook.com>
2019-07-15 11:58:10 +0800

    PID main

--- PID.py
+++ PID.py
@@ -5,9 +5,11 @@ def checkPID(pid):
 	except OSError: return False
 	else: return True
 
-def openFile(): f = open(os.path.expanduser("~") + "/pid.txt"); g = f.read(); f.close(); return json.loads(g)
+def openFile(): 
+	f = open(os.path.expanduser("~") + "/pid.txt"); g = f.read(); f.close(); return json.loads(g)
 
-def updtFile(v): u = open(os.path.expanduser("~") + "/pid.txt", "w"); u.write(v); u.close()
+def updtFile(v): 
+	u = open(os.path.expanduser("~") + "/pid.txt", "w"); u.write(v); u.close()
 
 def remCurrent(fname):
 	cList = openFile()
@@ -15,21 +17,21 @@ def remCurrent(fname):
 	except KeyError: pass
 	else: updtFile(json.dumps(cList))
 
-def addCurrent(fname, fnum): remCurrent(fname); updtFile(json.dumps(dict(openFile().items() + {fname: fnum}.items())))
-
-def checkAll():
-	cList = openFile(); keyList, valueList = list(), list()
-	if not len(cList): exit()
-	for key, value in cList.items():
-		keyList.append(key); valueList.append(value)
-	for k in range(0, len(keyList)):
-		cKey = keyList[k]; cValue = valueList[k]
-		if not checkPID(cValue):
-			print "PID " + str(cValue) + " [" + cKey + "] exit."
-			IFTTT.pushbots(
-				"Detected PID " + str(cValue) + ", refrenced to " + cKey + " exit.",
-				"Python Runtime Error", "", "raw", IFTTT.getkey()[0].split(), 0)
-			remCurrent(cKey)
+def addCurrent(fname, fnum): 
+	remCurrent(fname); updtFile(json.dumps(dict(openFile().items() + {fname: fnum}.items())))
 
 if __name__ == "__main__":
-	while True: checkAll(); print time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()); time.sleep(600)
+	while True:
+		cList = openFile(); keyList, valueList = list(), list()
+		if not len(cList): exit()
+		for key, value in cList.items():
+			keyList.append(key); valueList.append(value)
+		for k in range(0, len(keyList)):
+			cKey = keyList[k]; cValue = valueList[k]
+			if not checkPID(cValue):
+				print "PID " + str(cValue) + " [" + cKey + "] exit."
+				IFTTT.pushbots(
+					"Detected PID " + str(cValue) + ", refrenced to " + cKey + " exit.",
+					"Python Runtime Error", "", "raw", IFTTT.getkey()[0].split(), 0)
+				remCurrent(cKey)
+		print time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()); time.sleep(600)
