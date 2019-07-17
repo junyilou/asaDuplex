cc6ca19b20d206240cdb7244169ff881fe914252
Junyi Lou <junyi_lou@outlook.com>
2019-07-17 23:37:27 +0800

    useful updt

--- PID.py
+++ PID.py
@@ -11,6 +11,9 @@ def openFile():
 	except ValueError: h = json.loads("{}")
 	return h
 
+def updtFile(v): 
+	u = open(os.path.expanduser("~") + "/pid.txt", "w"); u.write(v); u.close()
+
 def remCurrent(fname):
 	cList = openFile()
 	try: cList.pop(fname)
@@ -18,8 +21,7 @@ def remCurrent(fname):
 	else: updtFile(json.dumps(cList))
 
 def addCurrent(fname, fnum): 
-	remCurrent(fname); u = open(os.path.expanduser("~") + "/pid.txt", "w")
-	u.write(json.dumps(dict(openFile().items() + {fname: fnum}.items()))); u.close()
+	remCurrent(fname); updtFile(json.dumps(dict(openFile().items() + {fname: fnum}.items())))
 
 if __name__ == "__main__":
 	while True:
