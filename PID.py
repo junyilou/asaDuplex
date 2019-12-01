067af200329118e0938165284422f879c06d0b3f
Junyi Lou <junyi_lou@outlook.com>
2019-12-01 13:58:39 +0800

    crontablize[3]

--- PID.py
+++ PID.py
@@ -1,4 +1,9 @@
-import os, json, time, IFTTT
+import os, json, time, logging, IFTTT
+
+logging.basicConfig(
+	filename = os.path.expanduser('~') + "/logs/" + os.path.basename(__file__) + ".log",
+	format = '[%(asctime)s %(levelname)s] %(message)s',
+	level = logging.DEBUG, filemode = 'w', datefmt = '%F %T %p')
 
 def checkPID(pid):
 	try: os.kill(pid, 0)
@@ -27,14 +32,13 @@ def addCurrent(fname, fnum):
 	updtFile(json.dumps(oList))
 
 if __name__ == "__main__":
-	while True:
-		cList = openFile()
-		if not len(cList): exit()
-		for key, value in cList.items():
-			if not checkPID(value):
-				print("PID " + str(value) + " [" + key + "] exit.")
-				IFTTT.pushbots(
-					"监测到 PID " + str(value) + " 对应的 " + key + " 已停止工作",
-					"Python 3 运行时错误", "", "raw", IFTTT.getkey()[0], 0)
-				remCurrent(key)
-		print(time.strftime("%F %T", time.localtime())); time.sleep(600)
+	cList = openFile()
+	if not len(cList): exit()
+	for key, value in cList.items():
+		if not checkPID(value):
+			print("PID " + str(value) + " [" + key + "] exit.")
+			IFTTT.pushbots(
+				"监测到 PID " + str(value) + " 对应的 " + key + " 已停止工作",
+				"Python 3 运行时错误", "", "raw", IFTTT.getkey()[0], 0)
+			remCurrent(key)
+	logging.info("This is the last time when this script ran.")
