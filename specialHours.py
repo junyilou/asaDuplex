045c1408affb8f2cc5f936b8611399d64385f96d
Junyi Lou <junyi_lou@outlook.com>
2020-02-08 16:21:43 +0800

    much simpler output

--- specialHours.py
+++ specialHours.py
@@ -1,4 +1,4 @@
-import json, datetime, os, logging, filecmp, time, difflib, IFTTT
+import json, datetime, os, logging, time, IFTTT
 from retailData import storename, storeID
 
 asaVersion = "5.7.0"
@@ -28,29 +28,12 @@ def ftup(raw):
 	else: detm = str(detm)
 	return (deth, detm)
 
-def tdet(a, b):
-	timeA = a[1] - a[0]
-	timeB = b[1] - b[0]
-	det = timeB - timeA
-	if ftup(det)[0] == "0": return ftup(det)[1] + "分钟"
-	elif ftup(det)[1] == "00": return ftup(det)[0] + "小时"
-	else: return ftup(det)[0] + "小时 " + ftup(det)[1] + "分钟"
-
-def fileOpen(fileloc):
-	try: 
-		with open(fileloc) as fin:
-			return fin.read()
-	except FileNotFoundError:
-		logging.error(fileloc + " 文件不存在")
-		return None
-
 def fileWrite(fileloc, writer): 
 	with open(fileloc, "w") as fout:
 		fout.write(writer)
 
 transdict = {"周一": 0, "周二": 1, "周三": 2, "周四": 3, "周五": 4, "周六": 5, "周日": 6}
 revtrans = ["周一", "周二", "周三", "周四", "周五", "周六", "周日"]
-specialist = "生成于 " + runtime + "\n\n"
 
 logging.info("正在确认远程 Apple Store app 版本...")
 os.system("wget -t 20 -T 5 -O " + rpath + "iTunesLookup https://itunes.apple.com/cn/lookup?id=375380948")
@@ -70,71 +53,59 @@ for i in storeID:
 	" --header 'X-MALang: zh-CN' " +
 	"'https://mobileapp.apple.com/mnr/p/cn/retail/storeDetails?storeNumber=R" + str(i) + "'")
 
+with open (rpath + "storeHours.json") as org: orgjson = json.loads(org.read())
+
+allSpecial = {"created": runtime}
+comparison = ""
+
 for sn, sid in zip(storename, storeID):
-	with open (rpath + "storeDeatils-R" + str(sid) + ".txt") as org: orgjson = org.read()
-	orgdict = json.loads(orgjson)["allStoreHoursMergedResponse"]
+	with open (rpath + "storeDeatils-R" + str(sid) + ".txt") as fin: storejson = fin.read()
+	storedict = json.loads(storejson)["allStoreHoursMergedResponse"]
 
-	regular = orgdict["regularHours"]
-	storeRegular = [(0, 0)] * 7
-	for r in regular:
-		rRange = r["range"].replace(":", "")
-		if r["time"] != "不营业":
-			rTime = r["time"].split(" – ")
-			rStart = tttf(rTime[0]); rEnd = tttf(rTime[1])
-			if rRange.count("周") == 1:
-				storeRegular[transdict[rRange]] = (rStart, rEnd)
-			else:
-				startDay = rRange.split(" – ")[0]; endDay = rRange.split(" – ")[1]
-				for d in range(transdict[startDay], transdict[endDay] + 1):
-					storeRegular[d] = (rStart, rEnd)
-	logging.info("取得 R" + str(sid) + " 的通常时间表")
-
-	try: special = orgdict["specialHours"]
+	try: special = storedict["specialHours"]
 	except: special = []
-	storeSpecial = ""
+	storeSpecial = {}
 	for s in special:
 		sWeekday = datetime.datetime.strptime(s["specialDate"], '%Y年%m月%d日').weekday()
-		if s["isClosed"] == "Y": sTime = (0, 0)
-		else: sTime = (tttf(s["startTime"]), tttf(s["endTime"]))
-		sRegular = storeRegular[sWeekday]
-		fRegular = ftup(sRegular[0])[0] + ":" + ftup(sRegular[0])[1] + "-" + ftup(sRegular[1])[0] + ":" + ftup(sRegular[1])[1]
-		fSpecial = ftup(sTime[0])[0] + ":" + ftup(sTime[0])[1] + "-" + ftup(sTime[1])[0] + ":" + ftup(sTime[1])[1]
-		singleSpecial = ("Apple " + sn + "(R" + str(sid) + ") 在 " + s["specialDate"] + "(" + revtrans[sWeekday] + ")"
-			+ " 的通常营业时间为 " + fRegular + "，特殊营业时间为 " + fSpecial + "，相差 " + tdet(sTime, sRegular)
-			+ "。事由和批注是 [" + s["reason"] + "]" + s.get("comments", "") + "\n")
-		storeSpecial += singleSpecial
-	specialist += storeSpecial
-	os.system("rm " + rpath + "storeDeatils-R" + str(sid) + ".txt")
-	logging.info("生成了 R" + str(sid) + " 的单一列表")
+		if s["isClosed"] == "Y": 
+			fSpecial = "CLOSED"
+		else: 
+			sTime = (tttf(s["startTime"]), tttf(s["endTime"]))
+			fSpecial = ftup(sTime[0])[0] + ":" + ftup(sTime[0])[1] + " - " + ftup(sTime[1])[0] + ":" + ftup(sTime[1])[1]
+		singleSpecial = {s["specialDate"]: fSpecial}
+		storeSpecial = {**storeSpecial, **singleSpecial}
+		try: 
+			orgSpecial = orgjson[str(sid)]["time"][s["specialDate"]]
+		except KeyError:
+			aOut = "Apple " + sn + " 在 " + s["specialDate"] + " 新增特别营业时间 " + fSpecial
+			comparison += aOut + "\n"; logging.info("监测到 " + aOut)
+		else: 
+			if orgSpecial != fSpecial:
+				aOut = "Apple " + sn + " 在 " + s["specialDate"] + " 的特别营业时间由 " + orgSpecial + " 改为 " + fSpecial
+				comparison += aOut + "\n"; logging.info("监测到 " + aOut)
+	if len(storeSpecial):
+		addSpecial = {sid: {"storename": sn, "time": storeSpecial}}
+		allSpecial = {**allSpecial, **addSpecial}
+	os.remove(rpath + "storeDeatils-R" + str(sid) + ".txt")
 
-newLocation = rpath + "storeHours-new.txt"
-listLoc = rpath + "storeHours.txt"
+jOut = json.dumps(allSpecial, ensure_ascii = False, indent = 2)
+os.system("mv " + rpath + "storeHours.json " + rpath + "storeHours-" + runtime + ".json")
+fileWrite(rpath + "storeHours.json", jOut)
+logging.info("写入新的 storeHours.json")
 
-fileWrite(newLocation, specialist)
-
-orgf = fileOpen(listLoc).split("\n"); rem = orgf[2:-1]; cor = orgf[:2]
-for m in rem:
-	mDate = datetime.datetime.strptime(m.split(") 在 ")[1].split("(周")[0], '%Y年%m月%d日')
-	if datetime.datetime.now() - mDate < datetime.timedelta(days = 1): cor.append(m)
-mWrite = "\n".join(cor) + "\n"
-fileWrite(listLoc, mWrite)
-
-fileLines = [specialist.split("\n")[2:], mWrite.split("\n")[2:]]
-if fileLines[0] != fileLines[1]:
-	logging.info("检测到有文件变化，正在生成 changeLog")
+if len(comparison):
+	tOut = "Generated on: " + runtime + "\n\nChanges:\n" + comparison + "\nOriginal JSON:\n" + jOut
 	fileDiff = '<!DOCTYPE html><head><meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>'
-	fileDiff += "storeHours changeLog " + runtime + "</title></head><body><pre><code>"
-	fileDiff += "Generated at " + runtime + " GMT+8\n"
-	for line in difflib.unified_diff(fileLines[1], fileLines[0]): fileDiff += line + "\n"
-	fileWrite("/root/www/storeHours.html", fileDiff + "</code></pre></body></html>")
-	os.system("mv " + listLoc + " " + listLoc.replace(".txt", "-" + runtime + ".txt"))
-	os.system("mv " + newLocation + " " + listLoc)
-	logging.info("文件生成完成，上一版本已保存至 storeHours-" + runtime + ".txt")
-	pushAns = "监测到有新的特别营业时间，点击链接查看 DIFF 内容。"
+	fileDiff += "storeHours " + runtime + "</title></head><body><pre><code>"
+	fileDiff += tOut + "</code></pre></body></html>"
+	fileWrite("/root/www/storeHours.html", fileDiff)
+	logging.info("文件生成完成，上一版本已保存至 storeHours-" + runtime + ".json")
+	pushAns = "监测到 Apple Store 零售店有 " + str(comparison.count("Apple")) + " 个特别营业时间变化，点击链接查看详细变化和原始 JSON 内容。"
 	logging.info("[运行结果] " + pushAns)
 	IFTTT.pushbots(pushAns, "https://www.apple.com/retail/store/flagship-store/drawer/michiganavenue/images/store-drawer-tile-1_small_2x.jpg",
 		"http://myv.ps/storeHours.html", "linkraw", IFTTT.getkey()[0], 0)
 else: 
-	os.system("mv " + newLocation + " " + listLoc)
+	os.remove(rpath + "storeHours-" + runtime + ".json")
 	logging.info("没有发现 storeHours 文件更新")
+
 logging.info("程序结束")
