014416df33ee4a8f4870139bf53a82a1ebd7a1f3
Junyi Lou <junyi_lou@outlook.com>
2020-04-16 16:14:51 +0800

    Rtl HEAD mode

--- Rtlimages.py
+++ Rtlimages.py
@@ -1,4 +1,4 @@
-import os, sys, json, time, logging, IFTTT
+import os, sys, json, time, logging, requests, IFTTT
 
 logging.basicConfig(
 	filename = os.path.expanduser('~') + "/logs/" + os.path.basename(__file__) + ".log",
@@ -6,19 +6,15 @@ logging.basicConfig(
 	level = logging.DEBUG, filemode = 'a', datefmt = '%F %T %p')
 
 def down(rtl, isSpecial):
-	spr = "R" + rtl + ".png"
-	sx = rpath + "Pictures/R" + rtl + ".png"
-	if os.path.isfile(sx): 
-		oldsize = os.path.getsize(sx)
-	else: 
-		oldsize = 0
-	os.system("wget -U ASA/" + asaVersion + " -t 100 -T 5 -N -P " + rpath + "Pictures/ " + dieter + spr)
-	if os.path.isfile(sx): 
-		newsize = os.path.getsize(sx)
-	else: 
-		newsize = 0
-	if newsize != oldsize and newsize > 1:
-		logging.info("监测到 R" + rtl + " 有更新")
+	base = dieter + "R" + rtl + ".png"
+	try: rmod = storejson['last'][rtl]
+	except KeyError: rmod = ""
+	r = requests.head(base, allow_redirects = True)
+	if r.status_code != 200: return 0
+	if r.headers['Last-Modified'] != rmod:
+		logging.info("监测到 R" + rtl + " 有更新，正在保存图片")
+		os.system("wget -O " + rpath + "R" + rtl + "_" +
+			r.headers['Last-Modified'].replace(" ", "").replace(",", "") + ".png " + base)
 		try: 
 			rname = storejson['name'][rtl]
 			rflag = storejson['flag'][rtl]
@@ -33,15 +29,14 @@ def down(rtl, isSpecial):
 			specialist.remove(rtl)
 			with open(rpath + "specialist.txt", "w") as w:
 				w.write(", ".join(specialist))
-		tellRaw = "零售店编号 R" + rtl + "，新图片的大小是 " + str(int(newsize / 1024)) + " KB。" + rnso
-		imageURL = dieter + spr + "?output-format=jpg"
+		tellRaw = "零售店编号 R" + rtl + "，新图片的最后修改标签是 " + r.headers['Last-Modified'] + "。" + rnso
 		titaru = rflag + "Apple " + rname + " 图片更新"
 		logging.info("[运行结果] " + titaru + "，" + tellRaw)
-		IFTTT.pushbots(tellRaw, titaru, imageURL, "raw", masterKey, 0)
+		IFTTT.pushbots(tellRaw, titaru, base + "?output-format=jpg", "raw", masterKey, 0)
 	elif isSpecial:
 		try: pname = "Apple " + storejson['name'][rtl] + " (R" + rtl + ")"
 		except KeyError: pname = "Apple Store (R" + rtl + ")"  
-		if newsize == 0: logging.info("检查到 " + pname + " 的图片服务器中不存在")
+		if r.status_code == 404: logging.info("检查到 " + pname + " 的图片服务器中不存在")
 		else: logging.info("检查到 "+ pname + " 的图片没有更新")
 
 totalStore = 901
