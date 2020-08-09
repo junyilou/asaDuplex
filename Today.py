c904b3e438f5b226bcd6be29baf0f2109bd8f386
Junyi Lou <junyi_lou@outlook.com>
2020-08-09 22:07:25 +0800

    resupport for HMT

--- Today.py
+++ Today.py
@@ -5,13 +5,15 @@ filename = ['qibao', 'shanghaiiapm', 'wujiaochang', 'nanjingeast', 'pudong', 'gl
 'sanlitun', 'chinacentralmall', 'chaoyangjoycity', 'wangfujing', 'xidanjoycity', 'mixcchengdu', 'taikoolichengdu', 'tianjinjoycity','riverside66tianjin',
 'mixctianjin', 'parc66jinan', 'mixcqingdao', 'parccentral','zhujiangnewtown', 'holidayplazashenzhen', 'mixcnanning', 'nanjingist', 'nanjingjinmaoplace', 
 'wondercity', 'center66wuxi', 'suzhou', 'mixczhengzhou', 'tianyisquare', 'mixchangzhou', 'westlake', 'xiamenlifestylecenter', 'tahoeplaza', 
-'olympia66dalian', 'parkland', 'zhongjiejoycity', 'mixcshenyang', 'jiefangbei', 'mixcchongqing', 'paradisewalkchongqing']
+'olympia66dalian', 'parkland', 'zhongjiejoycity', 'mixcshenyang', 'jiefangbei', 'mixcchongqing', 'paradisewalkchongqing',
+'ifcmall', 'festivalwalk', 'cantonroad', 'newtownplaza', 'apmhongkong', 'causewaybay', 'galaxymacau', 'cotaicentral', 'xinyia13', 'taipei101']
 
 storename = ['七宝', '上海环贸 iapm', '五角场', '南京东路', '浦东', '环球港', '香港广场', '昆明', 
 '三里屯', '华贸购物中心', '朝阳大悦城', '王府井', '西单大悦城', '成都万象城', '成都太古里', '天津大悦城', '天津恒隆广场', 
 '天津万象城', '济南恒隆广场', '青岛万象城', '天环广场', '珠江新城', '深圳益田假日广场', '南宁万象城', '南京艾尚天地', '南京金茂汇', 
 '虹悦城', '无锡恒隆广场', '苏州', '郑州万象城', '天一广场', '杭州万象城', '西湖', '厦门新生活广场', '泰禾广场', 
-'大连恒隆广场', '百年城', '中街大悦城', '沈阳万象城', '解放碑', '重庆万象城', '重庆北城天街']
+'大连恒隆广场', '百年城', '中街大悦城', '沈阳万象城', '解放碑', '重庆万象城', '重庆北城天街',
+'ifc mall', 'Festival Walk', 'Canton Road', 'New Town Plaza', 'apm Hong Kong', 'Causeway Bay', '澳門銀河', '金沙廣場', '信義 A13', '台北 101']
 
 rpath, wAns = os.path.expanduser('~') + "/Retail/", ""
 with open(rpath + "savedEvent.txt") as m: mark = m.read()
@@ -28,9 +30,13 @@ else:
 logging.info("程序启动")
 
 for fn in filename:
+	if fn == "qibao": region = "cn"
+	if fn == "ifcmall": region = "hk"
+	if fn == "galaxymacau": region = "mo"
+	if fn == "xinyia13": region = "tw"
 	logging.info("正在下载活动时间表文件: " + fn)
 	os.system("wget -t 20 -T 3 -O " + rpath + fn + ".json --no-check-certificate " +
-		"'https://www.apple.com/today-bff/landing/store?stageRootPath=/cn&storeSlug=" + fn + "'")
+		"'https://www.apple.com/today-bff/landing/store?stageRootPath=/" + region +"&storeSlug=" + fn + "'")
 
 for fn, cyn in zip(filename, storename):
 	with open(rpath + fn + ".json") as r:
