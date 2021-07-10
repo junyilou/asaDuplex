c6693cd9e46d353413254873683d23a622be5600
Junyi Lou <junyi_lou@outlook.com>
2021-07-10 19:14:15 +0800

    support for APU comments

--- special.py
+++ special.py
@@ -1,13 +1,39 @@
 import logging, requests, json
-from datetime import timedelta, date
-from storeInfo import storeInfo, storeURL, userAgent
-from constants import dayOfWeekENG
+from datetime import timedelta, date, datetime
+from storeInfo import storeInfo, storeURL, userAgent, storePage
+from constants import dayOfWeekENG, partSample
 
 def dateConvert(strdate):
 	year, month, day = strdate.split("-")
 	standard = date(int(year), 1, 1)
 	actual = standard + timedelta(days = int(day) - 1)
 	return actual
+
+def comment(sid):
+	try:
+		partNumber = f"MX0K2{partSample[storeInfo(sid)['flag']]}/A"
+	except IndexError:
+		return {}
+	baseURL = storeURL(sid).split('/retail')[0]
+	referer = {**userAgent, "Referer": f"{baseURL}/shop/product/{partNumber}"}
+	url = f"{baseURL}/shop/fulfillment-messages?searchNearby=false&parts.0={partNumber}&store=R{sid}"
+	
+	try:
+		r = requests.get(url, headers = referer).json()
+		j = r["body"]["content"]["pickupMessage"]["stores"]
+	except:
+		return {}
+
+	reason = {}
+	for s in j:
+		if s["storeNumber"] != f"R{sid}":
+			continue
+		for h in s["retailStore"]["storeHolidays"]:
+			sDay = datetime.strptime(h["date"], "%b %d")
+			sDay = date(date.today().year, sDay.month, sDay.day)
+			sTxt = (f"[{h['description']}]" if h["description"] else "") + (f" {h['comments']}" if h["comments"] else "")
+			reason[sDay] = sTxt
+	return reason
 
 def speHours(sid, r, mode = "special"):
 	url = storeURL(sid)
@@ -46,25 +72,33 @@ def speHours(sid, r, mode = "special"):
 				regularHours[dayOfWeekENG.index(day)] = f'{regular["opens"]} - {regular["closes"]}'
 	
 	specialHours = {}
-	for special in j["specialOpeningHoursSpecification"]:
-		validDates = []
-		startDate = max(date.today(), dateConvert(special["validFrom"]))
-		endDate = min(date.today() + timedelta(days = 13), dateConvert(special["validThrough"]))
-		while startDate <= endDate:
-			validDates.append(startDate)
-			startDate += timedelta(days = 1)
+	if j["specialOpeningHoursSpecification"]:
+		specialReasons = comment(sid)
+		for special in j["specialOpeningHoursSpecification"]:
+			validDates = []
+			startDate = max(date.today(), dateConvert(special["validFrom"]))
+			endDate = min(date.today() + timedelta(days = 13), dateConvert(special["validThrough"]))
+			while startDate <= endDate:
+				validDates.append(startDate)
+				startDate += timedelta(days = 1)
 
-		for validDate in validDates:
-			fRegular = regularHours[validDate.weekday()]
-			if "opens" not in special:
-				specialDict = {"regular": fRegular, "special": "已关闭"}
-			else:
-				fSpecial = f'{special["opens"]} - {special["closes"]}'
-				if fRegular == fSpecial:
-					break
-				specialDict = {"regular": fRegular, "special": fSpecial}
-			specialHours[str(validDate)] = specialDict
-	specialHours = dict(sorted(specialHours.items(), key = lambda k: k[0]))
+			for validDate in validDates:
+				if validDate in specialReasons:
+					reason = {"reason": specialReasons[validDate]}
+				else:
+					reason = {}
+				fRegular = regularHours[validDate.weekday()]
+				if "opens" not in special:
+					if fRegular == "已关闭":
+						continue
+					specialDict = {"regular": fRegular, "special": "已关闭", **reason}
+				else:
+					fSpecial = f'{special["opens"]} - {special["closes"]}'
+					if fRegular == fSpecial:
+						continue
+					specialDict = {"regular": fRegular, "special": fSpecial, **reason}
+				specialHours[str(validDate)] = specialDict
+		specialHours = dict(sorted(specialHours.items(), key = lambda k: k[0]))
 
 	if mode == "special":
 		return specialHours
