8b7d3989b446bbed38810655950e52f240a8fa69
Junyi Lou <15688661+junyilou@users.noreply.github.com>
2022-11-03 17:18:24 +0800

    rewritten Hours with Python 3.11 in mind

--- Hours.py
+++ Hours.py
@@ -1,139 +1,136 @@
-import json
 import os
-import logging
 import asyncio
-from datetime import datetime, date
+import json
+import logging
+from datetime import datetime
+from collections import OrderedDict
 
-from storeInfo import *
-from modules.special import speHours
+from storeInfo import storeReturn, dieterURL
+from modules.util import session_func, setLogger
+from modules.special import speHours, comment
 from modules.constants import DIFFHTML
-from modules.util import setLogger, session_func
 from sdk_aliyun import async_post
 from bot import chat_ids
 
-stdout = lambda p: print(datetime.now().strftime("[%F %T] ") + p)
-from sys import argv
-if "logging" in argv:
-	stdout = logging.info
-	argv.remove("logging")
-if len(argv) > 1:
-	include, exclude = " ".join(argv[1:]), ""
-else:
-	include, exclude = "🇨🇳", ""
+INCLUDE = "🇨🇳"
+EXCLUDE = "609"
+TODAY = (RUNTIME := datetime.now()).date()
 
-today = date.today()
-runtime = str(today)
-comparison = ""
-diffStore = []
-calendar = {}
+def sortOD(od):
+	res = OrderedDict()
+	for k, v in sorted(od.items()):
+		res[k] = sortOD(v) if isinstance(v, dict) else v
+	return res
 
-async def entry(session, sid, sn):
-	global orgjson, allSpecial, comparison, diffStore, calendar
-	specialHours = await speHours(session, sid)
-	stdout(f"处理 Apple {sn} (R{sid})")
-	storeDiff = ""
+async def entry(session, semaphore, sid, sn, saved):
+	async with semaphore:
+		special = await speHours(session = session, sid = sid, runtime = TODAY, askComment = False)
+	
+	diff = []
+	hours = saved | {"storename": sn} | special
 
-	if len(specialHours):
-		allSpecial[sid] = {"storename": sn} | allSpecial.get(sid, {}) | specialHours
+	for date in special:
+		if date not in saved:
+			diff.append(f"{'':8}{date}: 新增 {special[date]['special']}")
+		elif (svd := saved[date]["special"]) != (spe := special[date]["special"]):
+			diff.append(f"{'':8}{date}: 由 {svd} 改为 {spe}")
 
-	for s in specialHours:
-		fSpecial = specialHours[s]["special"]
-		if s in calendar:
-			calendar[s][sn] = fSpecial
-		else:
-			calendar[s] = {sn: fSpecial}
-		try: 
-			orgSpecial = orgjson[sid][s]["special"]
-		except KeyError:
-			storeDiff += f"{'':^8}{s}：新增 {fSpecial}\n"
-		else: 
-			if orgSpecial != fSpecial:
-				storeDiff += f"{'':^8}{s}：由 {orgSpecial} 改为 {fSpecial}\n"
+	for date in list(saved):
+		if date == "storename":
+			continue
+		dateobj = datetime.strptime(date, '%Y-%m-%d').date()
+		if dateobj < TODAY:
+			hours.pop(date)
+			continue
+		if date not in special:
+			diff.append(f"{'':8}{date}: 取消 {saved[date]['special']}")
 
-	if sid in orgjson:
-		for odate in list(orgjson[sid]):
-			if odate == "storename": 
-				continue
-			odatetime = datetime.strptime(odate, '%Y-%m-%d').date()
-			if odatetime < today: 
-				continue
-			try:
-				newSpecial = specialHours[odate]
-			except KeyError:
-				storeDiff += f"{'':^8}{odate}：取消 {orgjson[sid][odate]['special']}\n"
-				allSpecial[sid].pop(odate)
+	if diff:
+		logging.info(f"[Apple {sn}] " + ", ".join([i.lstrip() for i in diff]))
 
-	if len(storeDiff):
-		comp = f"{'':^4}Apple {sn}\n{storeDiff}"
-		comparison += comp
-		stdout(comp.strip())
-		diffStore.append(sn)
+	return {"hours": hours, "diff": diff}
 
 @session_func
 async def main(session):
-	global orgjson, allSpecial, comparison, diffStore, calendar
+	args = dict(needSplit = True, remove_closed = True, remove_future = True)
+	pref = dict(ensure_ascii = False, indent = 2)
+	
+	includes = storeReturn(INCLUDE, **args)
+	excludes = storeReturn(EXCLUDE, **args)
+	stores = [i for i in includes if i not in excludes]
+	workfile = "Retail/storeHours.json"
 
-	stores = storeReturn(include, needSplit = True, remove_closed = True, remove_future = True)
-	excludeStores = storeReturn(exclude, needSplit = True, remove_closed = True, remove_future = True)
-	stores = [i for i in stores if i not in excludeStores]
-
-	try: 
-		with open("Retail/storeHours.json") as o:
-			orgjson = json.loads(o.read())
-	except FileNotFoundError: 
-		orgjson = {}
-		with open("Retail/storeHours.json", "w") as w:
+	if os.path.isfile(workfile):
+		with open(workfile) as o:
+			saved = json.loads(o.read())
+	else:
+		saved = {}
+		with open(workfile, "w") as w:
 			w.write(r"{}")
 
-	allSpecial = orgjson | {"created": runtime}
-	tasks = [entry(session, sid, sn) for sid, sn in stores]
-	await asyncio.gather(*tasks)
+	semaphore = asyncio.Semaphore(20)
+	async with asyncio.TaskGroup() as tg:
+		tasks = {store: tg.create_task(
+			entry(session, semaphore, *store, saved.get(store[0], {})))
+		for store in stores}
 
-	for s in list(allSpecial):
-		if s == "created":
-			continue
-		saved = allSpecial[s]
-		for d in list(saved):
-			if d == "storename":
+	results, calendar = {}, {}
+	diffs, targets = [], []
+	comments = saved.get("comments", {})
+
+	for store, item in tasks.items():
+		sid, sn = store
+		result = item.result()
+		if len(result["hours"]) > 1:
+			results[sid] = result["hours"]
+		if result["diff"]:
+			diffs += [f"{'':4}Apple {sn}"] + result["diff"]
+			targets.append(store)
+		for date in result["hours"]:
+			if date == "storename":
 				continue
-			t = datetime.strptime(d, '%Y-%m-%d').date()
-			if t < today:
-				allSpecial[s].pop(d)
-		if len(saved) == 1:
-			allSpecial.pop(s)
+			calendar[date] = calendar.get(date, {})
+			calendar[date][sn] = result["hours"][date]["special"]
 
-	os.rename("Retail/storeHours.json", f"Retail/storeHours-{runtime}.json")
+	comm_store = [i[0] for i in targets]
+	while len(comm_store):
+		sid = comm_store.pop(0)
+		comm = await comment(session, sid)
+		for cid in comm:
+			comments[cid] = {d: k for d, k in comments.get(cid, {}).items() if datetime.strptime(d, '%Y-%m-%d').date() >= TODAY}
+			comments[cid] |= {datetime.strftime(d, '%Y-%m-%d'): k for d, k in comm[cid].items()}
+			_ = comm_store.remove(cid) if cid in comm_store else None
 
-	stdout("写入新的 storeHours.json")
-	jOut = json.dumps(allSpecial, ensure_ascii = False, indent = 2)
-	calendar = dict(sorted(calendar.items(), key = lambda k: k[0]))
-	calendar = json.dumps(calendar, ensure_ascii = False, indent = 2)
-	with open("Retail/storeHours.json", "w") as w:
-		w.write(jOut)
+	results, calendar, comments = map(sortOD, [results, calendar, comments])
+	output = {"update": RUNTIME.strftime("%F %T")} | results | {"comments": comments}
+	oldfile = workfile.replace(".json", f"-{RUNTIME.strftime('%y%m%d%H%M')}.json")
+	os.rename(workfile, oldfile)
+	logging.info("写入新的 storeHours.json")
+	with open(workfile, "w") as w:
+		w.write(json.dumps(output, ensure_ascii = False, indent = 2))
 
-	if len(comparison):
-		title = "Special Hours"
-		content = f"Apple Store 特别营业时间\n生成于 {runtime}\n\n\n变化:\n{comparison}\n\n日历:\n{calendar}\n\n\n原始 JSON:\n{jOut}"
-		pushStore = "Apple " + "、Apple ".join(diffStore[:2]) + (f" 等 {lendiff} 家零售店" if (lendiff := len(diffStore)) > 2 else "")
+	if not diffs:
+		logging.info("没有发现 storeHours 文件更新")
+		return os.remove(oldfile)
 
-		with open("/home/ecs-user/html/storeHours.html", "w") as w:
-			w.write(DIFFHTML.format(DIFFTITLE = title, DIFFCONTENT = content))
-		stdout("已生成对比文件 storeHours.html")
+	content = "Apple Store 特别营业时间\n生成于 {RUNTIME}\n\n变化:\n{DIFF}\n\n日历:\n{CALENDAR}\n\n原始 JSON:\n{JSON}".format(
+		RUNTIME = RUNTIME.strftime("%F %T"), DIFF = "\n".join(diffs),
+		CALENDAR = json.dumps(calendar, **pref), JSON = json.dumps(output, **pref))
+	with open("html/storeHours.html", "w") as w:
+		w.write(DIFFHTML.format(DIFFTITLE = "Special Hours", DIFFCONTENT = content))
+	logging.info("已生成对比文件 storeHours.html")
 
-		push = {
-			"mode": "photo-text",
-			"chat_id": chat_ids[0], 
-			"image": "https://www.apple.com/retail/store/flagship-store/drawer/michiganavenue/images/store-drawer-tile-1_medium_2x.jpg",
-			"text": f'*来自 Hours 的通知*\n{pushStore} 有特别营业时间更新 [↗](http://aliy.un/html/storeHours.html)',
-			"parse": 'MARK'
-		}
-		await async_post(push, logger = stdout)
-
-	else: 
-		os.remove(f"Retail/storeHours-{runtime}.json")
-		stdout("没有发现 storeHours 文件更新")
+	text = ", Apple ".join(["", *[i[1] for i in targets]]).removeprefix(", ")
+	push = {
+		"mode": "photo-text",
+		"chat_id": chat_ids[0],
+		"image": dieterURL(targets[0][0]).split("?")[0],
+		"text": f'*来自 Hours 的通知*\n{text} 有特别营业时间更新 [↗](http://aliy.un/html/storeHours.html)',
+		"parse": "MARK"
+	}
+	await async_post(push)
 
 setLogger(logging.INFO, os.path.basename(__file__))
-stdout("程序启动")
+logging.info("程序启动")
 asyncio.run(main())
-stdout("程序结束")
+logging.info("程序结束")
