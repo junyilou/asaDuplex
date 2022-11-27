49ee12f2785bd4a12637321abc72808d859e585b
Junyi Lou <15688661+junyilou@users.noreply.github.com>
2022-11-28 01:35:10 +0800

    Completely Rewritten storeInfo
    完全重写 storeInfo.py，带来面向对象思想的零售店对象，极大提升了易用性、可读性和运行效率，重写部分陈旧代码
    
    继续参考 @MichaelSteeber 的文件更新了 storeInfo.json，现按照零售店编号进行索引，引入了零售店状态等

--- storeInfo.py
+++ storeInfo.py
@@ -1,284 +1,241 @@
 import re
 import json
 from datetime import datetime
+from functools import total_ordering
+from modules.constants import allRegions, userAgent
 from modules.util import request
-from modules.constants import userAgent, allRegions
 
-with open("storeInfo.json") as r:
-	infoJSON = json.loads(r.read())
+STORES = {}
+DEFAULTFILE = "storeInfoObj.json"
 
-LIBRARY = {}
-Order = []
+@total_ordering
+class Store:
+	def __init__(self, sid, dct):
+		for e in ["name", "flag", "state", "city"]:
+			assert e in dct
 
-def StoreID(sid, fuzzy = False, include_coeff = False):
-	stores = []
-	if fuzzy:
-		sid = str(sid).upper().removeprefix("R")
-		ids = [i for i in LIBRARY]
-		for i in ids:
-			if sid in i:
-				coeff = (round(len(sid) / 3, 3), ) if include_coeff else ()
-				stores.append((i, actualName(infoJSON["name"][i]), *coeff))
-	else:
-		sid = f"{str(sid).upper().removeprefix('R'):0>3}"
-		if sid in LIBRARY:
-			coeff = (1.0, ) if include_coeff else ()
-			stores = [(sid, actualName(infoJSON["name"][sid]), *coeff)]
-	return stores
+		self.sid = f"{sid}"
+		self.rid = f"R{sid}"
+		self.iid = int(sid)
+		self.flag = dct["flag"]
 
-def StoreMatch(keyword, fuzzy = False, include_coeff = False):
-	stores = []
-	keyword = keyword.lower()
-	for i in LIBRARY:
-		for j in LIBRARY[i]:
-			k = j.lower()
-			if fuzzy:
-				if keyword in k:
-					coeff = (round(len(keyword) / len(k), 3), ) if include_coeff else ()
-					stores.append((i, actualName(infoJSON["name"][i]), *coeff))
-					break
+		self.name = dct["name"]
+		if isinstance(self.name, list):
+			self.altname = self.name
+			self.name = self.altname.pop(0)
+		else:
+			self.altname = []
+
+		if "timezone" in dct:
+			self.timezone = dct["timezone"]
+
+		if "dates" in dct:
+			self.dates = self.nso = dct["dates"]
+			if isinstance(self.nso, list):
+				self.nso = self.nso[0]
 			else:
-				if keyword == k:
-					coeff = (1.0, ) if include_coeff else ()
-					stores.append((i, actualName(infoJSON["name"][i])), *coeff)
-					break
+				self.dates = [self.dates]
+
+		if "modified" in dct:
+			self.modified = dct["modified"]
+
+		if "status" in dct:
+			self.isFuture = dct["status"] == "future"
+			self.isClosed = dct["status"] == "closed"
+			self.isIntern = dct["status"] == "internal"
+		else:
+			self.isFuture = self.isClosed = self.isIntern = False
+
+		self.state = dct["state"]
+		self.city = dct["city"]
+		self.region = allRegions[self.flag]
+		if "website" in dct:
+			self.slug = self.name.lower().replace(" ", "") if dct["website"] == "-" else dct["website"]
+			self.url = f"https://www.apple.com{self.region['storeURL']}/retail/{self.slug}"
+		
+		self.keys = ((dct["alter"].split(" ") if "alter" in dct else []) + 
+			[self.name, self.state, self.city, self.flag] + self.altname + 
+			([self.slug] if hasattr(self, "slug") else []) + 
+			[self.region["name"], self.region["nameEng"]] + self.region["altername"] + 
+			(["招聘", "Hiring"] if self.isFuture else []) + 
+			(["关闭", "Closed"] if self.isClosed else []))
+		self.keys += [k.replace(" ", "") for k in self.keys if " " in k]
+
+		self.sortkey = f"{self.flag} {self.state} {self.sid}"
+		self.raw = dct
+
+	def telename(self, bold = False, flag = False, sid = True):
+		bold = "*" if bold else ""
+		flag = f"{self.flag} " if flag else ""
+		sid = f" (R{self.sid})" if sid else ""
+		return f"{flag}{bold}Apple {self.name}{bold}{sid}"
+
+	def nsoString(self, userLang = True):
+		if not hasattr(self, "dates"):
+			return ""
+		
+		lang = {True: {"OPENED": "首次开幕于 {DATE}", "MOVED": "搬迁换址于 {DATE}", "CLOSED": "结束营业于 {DATE}", "FORMAT": "%Y 年 %-m 月 %-d 日"},
+			False: {"OPENED": "Opened on {DATE}", "MOVED": "Moved on {DATE}", "CLOSED": "Closed on {DATE}", "FORMAT": "%b %-d, %Y"}}
+		localize = lambda dt, userLang: datetime.strptime(dt, "%Y-%m-%d").strftime(lang[userLang]["FORMAT"])
+
+		info = [lang[userLang]["OPENED"].format(DATE = localize(self.dates[0], userLang))]
+		for d in self.dates[1 : -1 if self.isClosed else None]:
+			info.append(lang[userLang]["MOVED"].format(DATE = localize(d, userLang)))
+		if self.isClosed:
+			info.append(lang[userLang]["CLOSED"].format(DATE = localize(self.dates[-1], userLang)))
+		return "\n".join(info)
+
+	def __repr__(self):
+		return f"<Store {self.telename(flag = True)}>"
+
+	def __ge__(self, other):
+		assert isinstance(other, Store)
+		return self.sortkey >= other.sortkey
+
+	def __hash__(self):
+		return hash(self.sid)
+
+	async def detail(self, session = None, mode = "dict"):
+		try:
+			assert hasattr(self, "slug")
+			url = f"https://www.apple.com/rsp-web/store-detail?storeSlug={self.slug}&locale={self.region['rspLocale']}&sc=false"
+			if mode == "url":
+				return url
+
+			r = await request(session = session, url = url, headers = userAgent, 
+				ensureAns = False, retryNum = 3, timeout = 5, mode = "json")
+
+			hours = {
+				"isnso": r["hours"]["isNSO"],
+				"regular": r["hours"]["hoursData"],
+				"special": r["hours"]["specialHoursData"]}
+
+			match mode:
+				case "raw":
+					return r
+				case "hours":
+					return hours
+				case "dict":
+					add = r["address"]
+					address = add["address1"].rstrip(" ")
+					address += f', {add["address2"]}' if add["address2"] else ""
+					prov = add["city"]
+					prov += f', {add["stateName"]}' if add["stateName"] else ""
+					prov += f', {add["postal"]}' if add["postal"] else ""
+					info = {
+						"timezone": r["timezone"],
+						"telephone": r["telephone"],
+						"address": address,
+						"province": prov
+					}
+					return r["geolocation"] | info | hours
+		except:
+			return {}
+
+	@property
+	def dieter(self):
+		args = "crop=1"
+		'''
+			Akamai Image Server Refrence: 
+			https://developer.goacoustic.com/acoustic-content/docs/how-to-optimize-and-transform-your-images-automatically-with-akamai-1
+		'''
+		return f"https://rtlimages.apple.com/cmc/dieter/store/16_9/{self.rid}.png?{args}"
+
+	async def header(self, session = None):
+		try:
+			r = await request(session = session, url = self.dieter, headers = userAgent, ssl = False,
+				method = "HEAD", allow_redirects = False, raise_for_status = True, mode = "head", timeout = 5)
+			return r['Last-Modified'][5:-4]
+		except:
+			return None
+
+def StoreID(sid: int | str, fuzzy = False) -> list[Store]:
+	try:
+		assert sid
+		if fuzzy:
+			sid = str(sid).upper().removeprefix("R")
+			assert sid.isdigit()
+			stores = [i for i in STORES.values() if sid in i.sid]
+		else:
+			sid = f"{str(sid).upper().removeprefix('R'):0>3}"
+			assert sid.isdigit()
+			stores = [STORES[sid]] if sid in STORES else []
+	except AssertionError:
+		return []
 	return stores
 
-def actualName(name):
-	return name if type(name) == str else name[0]
-
-def storeInfo(sid):
-	sid = f"{sid}"
-	try:
-		sid = StoreID(sid)[0][0]
-		return {t: infoJSON[t][sid] for t in infoJSON if (sid in infoJSON[t]) and (type(infoJSON[t]) == dict)}
-	except IndexError:
-		return {}
-
-def storeURL(sid = None, sif = None, mode = None):
-	if sid:
-		sif = storeInfo(sid)
-	elif sif:
-		pass
+def StoreMatch(keyword: str, fuzzy = False) -> list[Store]:
+	if not keyword:
+		return []
+	if keyword == "all" and fuzzy:
+		return list(STORES.values())
+	if fuzzy:
+		stores = [i for i in STORES.values() if any([keyword.lower() in k.lower() for k in i.keys])]
 	else:
-		raise ValueError("Expect either `Store ID` or `sif dictionary` provided")
-	try:
-		website = actualName(sif["name"]).lower().replace(" ", "") if (website := sif["key"]["website"]) == "-" else website
-		return website if mode == "slug" else f"https://www.apple.com{allRegions[sif['flag']]['storeURL']}/retail/{website}"
-	except KeyError:
-		return ""
-
-async def storeDict(sid = None, sif = None, session = None, mode = "dict"):
-	try:
-		website = storeURL(sid = sid, sif = sif, mode = "slug")
-		assert website
-
-		sif = sif if sif != None else storeInfo(sid)
-		url = f"https://www.apple.com/rsp-web/store-detail?storeSlug={website}&locale={allRegions[sif['flag']]['rspLocale']}&sc=false"
-		if mode == "url":
-			return url
-
-		r = await request(session = session, url = url, headers = userAgent, ensureAns = False, retryNum = 3, timeout = 5)
-		r = json.loads(r)
-
-		hours = {
-			"isnso": r["hours"]["isNSO"],
-			"regular": r["hours"]["hoursData"],
-			"special": r["hours"]["specialHoursData"]
-		}
-
-		match mode:
-			case "raw":
-				return r
-			case "hours":
-				return hours
-			case "dict":
-				add = r["address"]
-				address = add["address1"].rstrip(" ")
-				address += f', {add["address2"]}' if add["address2"] else ""
-				prov = add["city"]
-				prov += f', {add["stateName"]}' if add["stateName"] else ""
-				prov += f', {add["postal"]}' if add["postal"] else ""
-				info = {
-					"timezone": r["timezone"],
-					"telephone": r["telephone"],
-					"address": address,
-					"province": prov
-				}
-				return r["geolocation"] | info | hours
-	except:
-		return {}
-
-def getCity(sid):
-	return infoJSON["key"][f"{str(sid).upper().removeprefix('R'):0>3}"]["city"]
-
-def getState(sid, stateOnly = False):
-	sid = f"{sid}"
-	state = infoJSON["key"][sid]["state"]
-	if stateOnly:
-		return state
-	else:
-		stores = [i for i in infoJSON["key"] if \
-			(infoJSON["key"][i]["state"] == state) and \
-			("Store in " not in infoJSON["name"][i])]
-		return state, stores
-
-def getNation(sid, userLang = None):
-	flag = infoJSON["flag"][f"{sid}"]
-	if userLang != None:
-		name = allRegions[flag]["name"] if userLang else allRegions[flag]["nameEng"]
-	else:
-		name = flag
-	return name, [i[0] for i in storeReturn(flag, remove_closed = True, remove_future = True)]
-
-def stateReplace(rstores, bold = False, number = True, userLang = None):
-	stores = rstores.copy()
-	if not stores:
-		return stores
-	while True:
-		for store in stores:
-			flag = False
-			if not store.isdigit():
-				continue
-			
-			nationName, nationStore = getNation(store, userLang = userLang)
-			if all([i in stores for i in nationStore]):
-				flag = True
-				deco = "*" if bold else ""
-				stores[stores.index(store)] = f"{deco}{nationName}{deco}" + (f" ({len(nationStore)})" if number else "")
-				_ = [stores.remove(j) for j in nationStore if j != store]
-			
-			stateName, stateStore = getState(store)
-			if all([i in stores for i in stateStore]):
-				stateLen = len(stateStore)
-				if stateLen != 1:
-					flag = True
-					deco = "*" if bold else ""
-					stores[stores.index(store)] = f"{deco}{stateName}{deco}" + (f" ({len(stateStore)})" if number else "")
-					_ = [stores.remove(j) for j in stateStore if j != store]
-			if flag:
-				break
-		if flag == False:
-			break
+		stores = [i for i in STORES.values() if keyword.lower() in [k.lower() for k in i.keys]]
 	return stores
 
-def storeReturn(args, sort = True, sort_coeff = False, 
-	remove_closed = False, remove_future = False, 
-	fuzzy = False, needSplit = False):
+def nameReplace(rstores: [Store], bold = False, number = True, final = "name", userLang = None) -> list[str]:
+	if not rstores:
+		return rstores
+	stores = set(rstores)
+	results = []
+	bold = "*" if bold else ""
+	
+	for store in stores:
+		for level in ["flag", "state", "city"]:
+			ast = set([s for s in STORES.values() if getattr(s, level) == getattr(store, level) and 
+				not (s.isClosed or s.isFuture or s.isIntern)])
+			if ast and ast.issubset(stores):
+				stores = stores.symmetric_difference(ast)
+				match level, userLang:
+					case "flag", None:
+						attr = store.flag
+					case "flag", True:
+						attr = store.region["name"]
+					case "flag", False:
+						attr = store.region["nameEng"]
+					case _, _:
+						attr = getattr(store, level)
+				num = f" ({len(ast)})" if number else ""
+				results.append(f"{bold}{attr}{bold}{num}")
+	results += [getattr(s, final) for s in stores]
+	return results
 
+def storeReturn(args: str | list, remove_closed = False, remove_future = False, 
+	fuzzy = False, split = False, sort = True) -> list[Store]:
 	ans = []
-	if needSplit:
-		args = re.split(",|，", "".join(args))
+	if split:
+		args = re.split(",|，", args)
 	if type(args) in [int, str]:
 		args = [f"{args}"]
 	for a in args:
-		if not (a := a.strip()):
-			continue
-		if a == "all":
-			a = ""
-		digit = a.isdigit() or a.upper().removeprefix("R").isdigit()
-		stores = (StoreID(a, fuzzy, sort_coeff) + StoreMatch(a, fuzzy, sort_coeff)) if digit else StoreMatch(a, fuzzy, sort_coeff)
+		a = a.strip()
+		stores = StoreID(str(a), fuzzy = fuzzy) + StoreMatch(str(a), fuzzy = fuzzy)
 		for s in stores:
-			if s and s not in ans:
-				sState = getState(s[0], stateOnly = True)
-				judge = (remove_future, remove_closed)
-				if any(judge):
-					if sState == "Internal":
-						continue
-					if judge[0] and "Store in" in infoJSON["name"][s[0]]:
-						continue
-					if judge[1] and sState == "Closed":
-						continue
+			if remove_closed and (s.isClosed or s.isIntern):
+				continue
+			if remove_future and (s.isFuture or s.isIntern):
+				continue
+			if s not in ans:
 				ans.append(s)
-
-	if sort_coeff:
-		ans = [i[:2] for i in sorted(ans, key = lambda k: k[2], reverse = True)]
-
-	elif sort:
-		order = {}
-		for store in ans:
-			sid = store[0]
-			try:
-				order[sid] = Order.index(sid)
-			except ValueError:
-				order[sid] = 900 + int(sid)
-		ans.sort(key = lambda k: order[k[0]])
-
+	if sort:
+		ans.sort(key = lambda k: k.sortkey)
 	return ans
 
-def dieterURL(sid, mode = 0):
-	digest = "crop=1" # "output-format=png"
-	'''
-		Akamai Image Server Refrence: 
-		https://developer.goacoustic.com/acoustic-content/docs/how-to-optimize-and-transform-your-images-automatically-with-akamai-1
-	'''
-	return f"https://rtlimages.apple.com/cmc/dieter/store/16_9/R{sid:0>3}.png?{digest}"
-
-async def DieterHeader(rtl, session = None):
-	sid = StoreID(rtl)
-	sid = sid[0][0] if sid != [] else rtl
-	try:
-		r = await request(session = session, url = dieterURL(sid), headers = userAgent, ssl = False,
-			method = "HEAD", allow_redirects = False, raise_for_status = True, mode = "head", retryNum = 3, timeout = 5)
-		return r['Last-Modified'][5:-4]
-	except:
-		return None
-
-def nsoString(sid = None, sif = None, userLang = True):
-	if sid:
-		sif = storeInfo(sid)
-	elif sif:
-		pass
-	else:
-		raise ValueError("Expect either `Store ID` or `sif dictionary` provided")
-
-	lang = {True: {"OPENED": "首次开幕于 {DATE}", "MOVED": "搬迁换址于 {DATE}", "CLOSED": "结束营业于 {DATE}", "FORMAT": "%Y 年 %-m 月 %-d 日"},
-		False: {"OPENED": "Opened on {DATE}", "MOVED": "Moved on {DATE}", "CLOSED": "Closed on {DATE}", "FORMAT": "%b %-d, %Y"}}
-	localize = lambda dt, userLang: datetime.strptime(dt, "%Y-%m-%d").strftime(lang[userLang]["FORMAT"])
-
-	info = []
-	if "nso" in sif:
-		if type(sif["nso"]) == str:
-			return lang[userLang]["OPENED"].format(DATE = localize(sif["nso"], userLang))
-		
-		info.append(lang[userLang]["OPENED"].format(DATE = localize(sif["nso"][0], userLang)))
-		for d in sif["nso"][1 : -1 if sif["key"]["state"] == "Closed" else None]:
-			info.append(lang[userLang]["MOVED"].format(DATE = localize(d, userLang)))
-		if sif["key"]["state"] == "Closed":
-			info.append(lang[userLang]["CLOSED"].format(DATE = localize(sif["nso"][-1], userLang)))
-		return "\n".join(info)
-	return ""
-
-def library():
-	global LIBRARY, Order
-	LIBRARY = {}
-	for i in infoJSON["name"]:
-		comp = [infoJSON["name"][i]] if type(infoJSON["name"][i]) == str else infoJSON["name"][i].copy()
-		LIBRARY[i] = comp.copy()
-		for j in comp:
-			if " " in j:
-				LIBRARY[i].append(j.replace(" ", ""))
-	for i in infoJSON["key"]:
-		keys = []
-		for j in infoJSON["key"][i]:
-			k = infoJSON["key"][i][j]
-			match j:
-				case "website":
-					keys.append(actualName(infoJSON["name"][i]).lower().replace(" ", "") if k == "-" else k)
-				case "alter":
-					keys += k.split(" ")
-				case _:
-					keys += [k] + ([k.replace(" ", "")] if " " in k else [])
-		LIBRARY[i] = LIBRARY.get(i, []) + keys
-	for i in infoJSON["flag"]:
-		flag = infoJSON["flag"][i]
-		LIBRARY[i] = LIBRARY.get(i, []) + [flag, allRegions[flag]["name"], allRegions[flag]["nameEng"]] + allRegions[flag]["altername"]
-	Order = sorted([i for i in infoJSON["key"]], key = lambda k: f'{infoJSON["flag"][k]} {infoJSON["key"][k]["state"]}')
-
-library()
-def reloadJSON(filename = "storeInfo.json"):
-	global infoJSON
+def reloadJSON(filename = DEFAULTFILE) -> str:
+	global STORES
 	with open(filename) as r:
-		infoJSON = json.loads(r.read())
-	library()
+		infoJSON = json.load(r)
+	for sid, dct in infoJSON.items():
+		if sid == "update":
+			continue
+		STORES[sid] = Store(sid = sid, dct = dct)
+	STORES = {k: v for k, v in sorted(STORES.items(), key = lambda s: s[1].sortkey)}
 	return infoJSON["update"]
+
+def getStore(sid) -> Store:
+	f = f"{str(sid).upper().removeprefix('R'):0>3}"
+	return STORES[f] if f in STORES else None
+
+reloadJSON()
