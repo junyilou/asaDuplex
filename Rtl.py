604f199105988565de32201b5c47f45ff93a965a
Junyi Lou <15688661+junyilou@users.noreply.github.com>
2022-11-28 01:36:21 +0800

    Rewritten Rtl with Object-Orient
     部分重写 Rtl 以适应新的 storeInfo，更新部分陈旧格式和调用方式，提升搜索和运行效率

--- Rtl.py
+++ Rtl.py
@@ -7,60 +7,48 @@ from base64 import b64encode
 from datetime import datetime, date, UTC
 
 from bot import chat_ids
-from storeInfo import storeInfo, actualName, dieterURL, DieterHeader, nsoString
+from storeInfo import DEFAULTFILE, StoreID
 from modules.constants import userAgent
 from modules.util import request, disMarkdown, setLogger, session_func
 from sdk_aliyun import async_post
 
-specialist = []
 INVALIDDATE = datetime(2001, 5, 19)
 INVALIDREMOTE = [date(2021, 7, 13), date(2021, 8, 28), date(2021, 8, 29), date(2022, 1, 7)]
 
-with open("storeInfo.json") as s:
-	storejson = json.loads(s.read())
-
-async def down(session, rtl, isSpecial, semaphore = None):
-	global specialist
-	rtl = f"{rtl:0>3}"
-	
-	try: 
-		saved = storejson['last'][rtl]
+async def down(session, sid, storejson, specialist, semaphore):
+	store = StoreID(sid)[0]
+	if hasattr(store, "modified"):
+		saved = store.modified
 		savedDatetime = datetime.strptime(saved, "%d %b %Y %H:%M:%S")
-	except KeyError:
-		saved = None
-		savedDatetime = None
+	else:
+		saved = savedDatetime = None
 	try:
-		if semaphore != None:
-			await semaphore.acquire()
-		if rtl.endswith("00"):
-			logging.info(f"开始下载 R{rtl}")
-		remote = await DieterHeader(rtl = rtl, session = session)
+		async with semaphore:
+			if store.sid.endswith("00"):
+				logging.info(f"开始下载 {store.rid}")
+			remote = await store.header(session = session)
 		remoteDatetime = datetime.strptime(remote, "%d %b %Y %H:%M:%S")
-	except TypeError:
-		remoteDatetime = None
-	finally:
-		if semaphore != None:
-			semaphore.release()
+	except:
+		remote = remoteDatetime = None
 
 	if not remoteDatetime:
-		if isSpecial:
-			logging.info(f"R{rtl} 文件不存在或获取失败")
+		if specialist != None:
+			logging.info(f"{store.rid} 文件不存在或获取失败")
 		return False
 	elif not savedDatetime:
 		savedDatetime = INVALIDDATE
 
 	if remoteDatetime > savedDatetime:
+		storejson[store.sid]["modified"] = remote
 		if remoteDatetime.date() in INVALIDREMOTE:
-			logging.info(f"R{rtl} 找到了更佳的远端无效时间")
-			storejson['last'][rtl] = remote
-			storejson['update'] = datetime.now(UTC).strftime("%F %T GMT")
+			logging.info(f"{store.rid} 找到了更佳的远端无效时间")
 			return True
 
-		logging.info(f"R{rtl} 更新，标签为 {remote}")
-		savename = f"Retail/R{rtl}_{remote.replace(' ', '').replace(':', '')}.png"
+		logging.info(f"{store.rid} 更新，标签为 {remote}")
+		savename = f"Retail/{store.rid}_{remote.replace(' ', '').replace(':', '')}.png"
 
 		try:
-			r = await request(session = session, url = dieterURL(rtl), headers = userAgent, 
+			r = await request(session = session, url = store.dieter, headers = userAgent, 
 				ssl = False, mode = "raw", ensureAns = False)
 			with open(savename, "wb") as w:
 				w.write(r)
@@ -68,29 +56,18 @@ async def down(session, rtl, isSpecial, semaphore = None):
 			logging.error(f"下载文件到 {savename} 失败")
 			pass
 
-		sif = storeInfo(rtl)
-		name = actualName(sif["name"])
-		info = [f"*{sif['flag']} Apple {name}* (R{rtl})", "", f"*远程标签* {remote}"]
-		if "nso" in sif:
-			info.insert(1, nsoString(sif = sif))
+		info = [store.telename(flag = True, bold = True, sid = True), "", f"*远程标签* {remote}"]
+		if hasattr(store, "dates"):
+			info.insert(1, store.nsoString())
 		if saved:
 			info.insert(-1, f"*本地标签* {saved}")
 		info = "\n".join(info)
 	
-		if isSpecial: 
-			logging.info("正在更新 specialist.txt")
-			specialist.remove(str(int(rtl)))
-			with open("Retail/specialist.txt", "w") as w:
-				w.write(", ".join(specialist))
+		if specialist != None:
+			toPop = str(store.iid)
+			_ = specialist.remove(toPop) if toPop in specialist else None
 
-		storejson['last'][rtl] = remote
-		if savedDatetime == INVALIDDATE:
-			storejson["last"] = dict(sorted(storejson["last"].items(), key = lambda k: k[0]))
-
-		storejson['update'] = datetime.now(UTC).strftime("%F %T GMT")
-
-		with open(savename, "rb") as r:
-			img = b64encode(r.read()).decode()
+		img = b64encode(r).decode()
 		push = {
 			"chat_id": chat_ids[0],
 			"mode": "photo-text",
@@ -99,59 +76,54 @@ async def down(session, rtl, isSpecial, semaphore = None):
 			"parse": "MARK"
 		}
 		await async_post(push)
-
 		return True
-
-	elif isSpecial:
-		logging.info(f"R{rtl} 图片没有更新")
+	elif specialist != None:
+		logging.info(f"{store.rid} 图片没有更新")
 	return False
 
 @session_func
 async def main(session):
-	global specialist
 	semaphore = asyncio.Semaphore(50)
+	with open(DEFAULTFILE) as r:
+		storejson = json.load(r)
 
-	match sys.argv:
-		case [_]:
-			print("请指定一种运行模式: normal, special 或 single")
-			return
-		case [_, "normal"]:
+	match sys.argv[1:]:
+		case []:
+			return print("请指定一种运行模式: normal, special 或 single")
+		case ["normal" | "single" as mode, *targets]:
 			setLogger(logging.INFO, os.path.basename(__file__))
-			logging.info("开始枚举零售店")
-				
-			runFlag = False
-			tasks = [down(session, f"{j:0>3d}", False, semaphore) for j in range(1, 901)]
-			runFlag |= any(await asyncio.gather(*tasks))
-
-		case [_, "special"]:
-			with open("Retail/specialist.txt") as l:
-				specialist = eval(f"[{l.read()}]")
+			if mode == "normal":
+				targets = [i for i in storejson if i != "update"]
+			logging.info(f"开始查询 {len(targets)} 家零售店")
+			tasks = [down(session, i, storejson, None, semaphore) for i in targets]
+			runFlag = any(await asyncio.gather(*tasks))
+		case ["special"]:
+			with open("Retail/specialist.txt") as r:
+				specialist = eval(f"[{r.read()}]")
 			specialist = [str(i) for i in specialist]
+			locallist = specialist.copy()
 
 			if len(specialist):
 				setLogger(logging.INFO, os.path.basename(__file__))
 				logging.info("开始特别观察模式: " + ", ".join(specialist))
-
-				tasks = [down(session, i, True, semaphore) for i in specialist]
+				tasks = [down(session, i, storejson, specialist, semaphore) for i in locallist]
 				runFlag = any(await asyncio.gather(*tasks))
+				if locallist != specialist:
+					logging.info("正在更新特别观察列表")
+					with open("Retail/specialist.txt", "w") as w:
+						w.write(str(list(map(int, specialist))).strip("[]"))
 			else:
 				return
-
-		case [_, "single", *targets]:
-			setLogger(logging.INFO, os.path.basename(__file__))
-			logging.info("开始单独调用模式: " + ", ".join(targets))
-			tasks = [down(session, i, False, semaphore) for i in targets]
-			runFlag = any(await asyncio.gather(*tasks))
-
-		case [_, mode, *_]:
-			print("指定了错误的运行模式: normal, special 或 single")
-			return
+		case _:
+			return print("指定了错误的运行模式: normal, special 或 single")
 
 	if runFlag:
-		logging.info("正在更新 storeInfo.json")
+		logging.info(f"正在更新 {DEFAULTFILE}")
+		storejson["update"] = datetime.now(UTC).strftime("%F %T GMT")
 		sOut = json.dumps(storejson, ensure_ascii = False, indent = 2)
-		with open("storeInfo.json", "w") as sw:
-			sw.write(sOut)
+		with open(DEFAULTFILE, "w") as w:
+			w.write(sOut)
+
+	logging.info("程序结束")
 
 asyncio.run(main())
-logging.info("程序结束")
