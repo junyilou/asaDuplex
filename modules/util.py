b9d9be42b9351ee8c506dbd69a18a78635c0f31a
Junyi Lou <15688661+junyilou@users.noreply.github.com>
2023-01-10 20:54:34 +0800

    general timeDelta func

--- modules/util.py
+++ modules/util.py
@@ -3,7 +3,7 @@ import asyncio
 import json
 import logging
 from datetime import datetime
-from typing import Callable, Awaitable
+from typing import Awaitable, Callable, Optional
 from os.path import isdir
 
 def bitsize(integer: int | float, width: int = 8, precision: int = 2, ks: float = 1e3) -> str:
@@ -21,7 +21,7 @@ def disMarkdown(text: str, wrap: str = "", extra: str = "") -> str:
 		temp = temp.replace(s, "\\" + s)
 	return wrap + temp + wrap[::-1]
 
-def sortOD(od: dict, reverse: list[bool] = [False], key: Callable = None, level: int = 0) -> dict:
+def sortOD(od: dict, reverse: list[bool] = [False], key: Optional[Callable] = None, level: int = 0) -> dict:
 	res = {}
 	for k, v in sorted(od.items(), reverse = reverse[min(level, len(reverse) - 1)], key = key):
 		match v:
@@ -33,13 +33,27 @@ def sortOD(od: dict, reverse: list[bool] = [False], key: Callable = None, level:
 				res[k] = v
 	return res
 
+def timeDelta(*, seconds: int = 0, dt1: Optional[datetime] = None, dt2: Optional[datetime] = None, items: int = 5) -> str:
+	ans, base = [], 1
+	s = seconds or (dt2 - dt1).total_seconds()
+	comp = [(60, "秒"), (60, "分钟"), (24, "小时"), (7, "天"), (0, "周")]
+	for carry, desc in comp:
+		if (c := s // base):
+			if (l := f"{c % carry if carry else c:.0f}") != "0":
+				ans.append(f"{l} {desc}")
+		base *= carry
+		if s < base:
+			break
+	return " ".join(ans[-1:-1-items:-1])
+
 def timezoneText(dtime: datetime) -> str:
 	delta = dtime.utcoffset().total_seconds() / 3600
 	dx, dy = str(delta).split(".")
 	return f"GMT{int(dx):+}" + (f":{60 * float('.' + dy):0>2.0f}" if dy != "0" else "")
 
-async def request(session: aiohttp.ClientSession = None, url: str = None, mode: str | list[str] = None, 
+async def request(session: Optional[aiohttp.ClientSession] = None, url: str = "", mode: Optional[str | list[str]] = None,
 	retryNum: int = 1, ensureAns: bool = True, **kwargs) -> int | str | dict | bytes | Exception:
+	assert url != "", "URL 不合法"
 	method = kwargs.get("method", "GET")
 	pop = kwargs.pop("method") if "method" in kwargs else None
 	logger = logging.getLogger("async_request")
@@ -50,7 +64,7 @@ async def request(session: aiohttp.ClientSession = None, url: str = None, mode: 
 		session = aiohttp.ClientSession()
 		close_session = True
 
-	mode = [mode] if type(mode) != list else mode
+	mode: list[str] = [mode] if type(mode) != list else mode
 	logger.debug(f"[{method}] '{url}', [模式] {mode}, [参数] {kwargs}, [重试] {retryNum}")
 	while retryNum:
 		try:
@@ -98,7 +112,7 @@ def session_func(func):
 			return await func(session = session, *args, **kwargs)
 	return wrapper
 
-def sync(coroutine: Awaitable = None, loop: asyncio.AbstractEventLoop = None):
+def sync(coroutine: Optional[Awaitable] = None, loop: Optional[asyncio.AbstractEventLoop] = None):
 	if loop is None:
 		loop = asyncio.new_event_loop()
 	if coroutine is not None:
