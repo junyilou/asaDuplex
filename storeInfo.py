c4ed78a60f752331ef425dd0ad25ed01a006f72a
Junyi Lou <15688661+junyilou@users.noreply.github.com>
2023-11-18 22:44:13 +0800

    new: multiple mod in base module
    rem unnecessary type hints
    change loc of ">" in repr str
    deprecate hero and url in detail mode
    deprecate overloading
    fix function def orders
    storeReturn support custom filter func

--- storeInfo.py
+++ storeInfo.py
@@ -1,9 +1,9 @@
 import json
 import re
 from datetime import datetime
-from modules.constants import Region, Regions, userAgent
+from modules.constants import Regions, userAgent
 from modules.util import SessionType, request
-from typing import Any, Literal, Optional, Required, TypedDict, overload
+from typing import Any, Callable, Literal, Optional, Required, TypedDict
 
 DEFAULTFILE = "storeInfo.json"
 
@@ -23,10 +23,10 @@ STORES: dict[str, "Store"] = {}
 
 class Store:
 	def __init__(self, sid: int | str, dct: StoreDict) -> None:
-		self.sid: str = f"{sid:0>3}"
-		self.rid: str = "R" + self.sid
-		self.iid: int = int(self.sid)
-		self.flag: str = dct["flag"]
+		self.sid = f"{sid:0>3}"
+		self.rid = "R" + self.sid
+		self.iid = int(self.sid)
+		self.flag = dct["flag"]
 
 		name = dct["name"]
 		if isinstance(name, list):
@@ -46,59 +46,46 @@ class Store:
 				self.nso: str = raw_dates
 
 		if "timezone" in dct:
-			self.timezone: str = dct["timezone"]
+			self.timezone = dct["timezone"]
 		if "modified" in dct:
-			self.modified: str = dct["modified"]
+			self.modified = dct["modified"]
 
 		status = dct.get("status")
 		trans_table = {"closed": "关闭", "future": "招聘", "internal": "内部"}
-		self.isClosed: bool = status == "closed"
-		self.isFuture: bool = status == "future"
-		self.isIntern: bool = status == "internal"
-		self.isNormal: bool = not any([self.isClosed, self.isFuture, self.isIntern])
+		self.isClosed = status == "closed"
+		self.isFuture = status == "future"
+		self.isIntern = status == "internal"
+		self.isNormal = not (self.isClosed or self.isFuture or self.isIntern)
 
-		self.state: str = dct["state"]
-		self.city: str = dct["city"]
-		self.region: Region = Regions[self.flag]
+		self.state = dct["state"]
+		self.city = dct["city"]
+		self.region = Regions[self.flag]
 		if "website" in dct:
-			self.slug: str = dct["website"] or self.name.lower().replace(" ", "")
-			self.url: str = f"https://www.apple.com{self.region.url_retail}/retail/{self.slug}"
+			self.slug = dct["website"] or self.name.lower().replace(" ", "")
+			self.url = f"https://www.apple.com{self.region.url_retail}/retail/{self.slug}"
+			self.detail_url = f"https://www.apple.com/rsp-web/store-detail?storeSlug={self.slug}&locale={self.region.locale}&sc=false"
 
-		keys = [
+		keys: list[str] = [
 			self.name, self.state, self.city, self.flag, *self.altname,
 			*dct.get("alter", "").split(" "),
 			getattr(self, "slug", ""),
 			self.region.name, self.region.name_eng, self.region.abbr, *self.region.name_alter,
 			*([dct["status"].capitalize(), trans_table[dct["status"]]] if "status" in dct else [])]
-		self.keys: list[str] = [i for i in keys if i]
+		self.keys = [i for i in keys if i]
 		self.keys += [k.replace(" ", "") for k in self.keys if " " in k]
 
-		self.sortkey: tuple[str, str, str] = (self.flag, self.state, self.sid)
+		self.sortkey = (self.flag, self.state, self.sid)
 		self.raw: dict[str, Any] = dict(dct)
 
-	def telename(self, bold: bool = False, flag: bool = False, sid: bool = True) -> str:
-		c = ((flag, f"{self.flag} "), (bold, "*"), (True, f"Apple {self.name}"), (bold, "*"), (sid, f" ({self.rid})"))
-		return "".join(j for i, j in c if i)
-
-	def nsoString(self, userLang: bool = True) -> str:
-		if not hasattr(self, "dates"):
-			return ""
-
-		lang = {True: {"OPENED": "首次开幕于 {DATE}", "MOVED": "搬迁换址于 {DATE}", "CLOSED": "结束营业于 {DATE}", "FORMAT": "%Y 年 %-m 月 %-d 日"},
-			False: {"OPENED": "Opened on {DATE}", "MOVED": "Moved on {DATE}", "CLOSED": "Closed on {DATE}", "FORMAT": "%b %-d, %Y"}}
-		localize = lambda dt, userLang: datetime.strptime(dt, "%Y-%m-%d").strftime(lang[userLang]["FORMAT"])
-
-		info = [lang[userLang]["OPENED"].format(DATE = localize(self.dates[0], userLang))]
-		for d in self.dates[1 : -1 if self.isClosed else None]:
-			info.append(lang[userLang]["MOVED"].format(DATE = localize(d, userLang)))
-		if self.isClosed:
-			info.append(lang[userLang]["CLOSED"].format(DATE = localize(self.dates[-1], userLang)))
-		return "\n".join(info)
+	@property
+	def dieter(self) -> str:
+		args = "crop=1" # https://developer.goacoustic.com/acoustic-content/docs/how-to-optimize-and-transform-your-images-automatically-with-akamai-1
+		return f"https://rtlimages.apple.com/cmc/dieter/store/16_9/{self.rid}.png?{args}"
 
 	def __repr__(self) -> str:
-		name = [f"<Store {self.telename(flag = True)}>"]
+		name = [f"Store {self.telename(flag = True)}"]
 		status = [f"({s[2:].capitalize()})" for s in ["isClosed", "isFuture", "isIntern"] if getattr(self, s)]
-		return " ".join(name + status)
+		return f"<{' '.join(name + status)}>"
 
 	def __str__(self) -> str:
 		return self.telename(sid = False)
@@ -116,59 +103,51 @@ class Store:
 	def __hash__(self) -> int:
 		return hash(self.sortkey)
 
-	@overload
-	async def detail(self, mode: Optional[Literal["dict", "hours", "raw"]] = None, session: Optional[SessionType] = None) -> dict[str, Any]: ...
-	@overload
-	async def detail(self, mode: Literal["hero", "url"], session: Optional[SessionType] = None, ) -> str: ...
-	async def detail(self, mode: Optional[str] = None, session: Optional[SessionType] = None) -> dict[str, Any] | str:
+	async def detail(self,
+		mode: Optional[Literal["dict", "hours", "raw"]] = None,
+		session: Optional[SessionType] = None) -> dict[str, Any]:
 		try:
-			assert mode in ["dict", "hero", "hours", "raw", "url", None]
-			assert hasattr(self, "slug")
-			url = f"https://www.apple.com/rsp-web/store-detail?storeSlug={self.slug}&locale={self.region.locale}&sc=false"
-			if mode == "url":
-				return url
-
-			r = await request(session = session, url = url, headers = userAgent,
+			assert hasattr(self, "detail_url")
+			r = await request(session = session, url = self.detail_url, headers = userAgent,
 				ensureAns = False, retryNum = 3, timeout = 5, mode = "json")
-			hours = {
-				"isnso": r["hours"]["isNSO"],
-				"regular": r["hours"]["hoursData"],
-				"special": r["hours"]["specialHoursData"]}
-
-			match mode:
-				case "raw":
-					return r
-				case "hero":
-					return r["heroImage"]["large"]["x2"]
-				case "hours":
+			if mode == "raw":
+				return r
+			else:
+				hours = {"isnso": r["hours"]["isNSO"], "regular": r["hours"]["hoursData"], "special": r["hours"]["specialHoursData"]}
+				if mode == "hours":
 					return hours
-				case _:
-					add = r["address"]
-					address = ", ".join(a.strip() for a in [add["address1"], add["address2"]] if a)
-					province = ", ".join(a.strip() for a in [add["city"], add["stateName"], add["postal"]] if a)
-					info = {"timezone": r["timezone"], "telephone": r["telephone"], "address": address, "province": province}
-					return r["geolocation"] | info | hours
+				add = r["address"]
+				address = ", ".join(a.strip() for a in [add["address1"], add["address2"]] if a)
+				province = ", ".join(a.strip() for a in [add["city"], add["stateName"], add["postal"]] if a)
+				info = {"timezone": r["timezone"], "telephone": r["telephone"], "address": address, "province": province}
+				return r["geolocation"] | info | hours
 		except:
-			return "https://www.apple.com" if mode in ["hero", "url"] else {}
-
-	@property
-	def dieter(self) -> str:
-		args = "crop=1"
-		'''
-			Akamai Image Server Refrence:
-			https://developer.goacoustic.com/acoustic-content/docs/how-to-optimize-and-transform-your-images-automatically-with-akamai-1
-		'''
-		return f"https://rtlimages.apple.com/cmc/dieter/store/16_9/{self.rid}.png?{args}"
+			return {}
 
 	async def header(self, session: Optional[SessionType] = None) -> Optional[str]:
 		try:
 			r = await request(session = session, url = self.dieter, headers = userAgent, ssl = False,
-				method = "HEAD", allow_redirects = False, raise_for_status = True, mode = "head", timeout = 5)
+				method = "HEAD", allow_redirects = False, raise_for_status = True, mode = "HEAD", timeout = 5)
 			return r['Last-Modified'][5:-4]
 		except:
 			return None
 
-STORES: dict[str, Store] = {}
+	def nsoString(self, userLang: bool = True) -> str:
+		if not hasattr(self, "dates"):
+			return ""
+		lang = {True: {"OPENED": "首次开幕于 {DATE}", "MOVED": "搬迁换址于 {DATE}", "CLOSED": "结束营业于 {DATE}", "FORMAT": "%Y 年 %-m 月 %-d 日"},
+			False: {"OPENED": "Opened on {DATE}", "MOVED": "Moved on {DATE}", "CLOSED": "Closed on {DATE}", "FORMAT": "%b %-d, %Y"}}
+		localize = lambda dt, userLang: datetime.strptime(dt, "%Y-%m-%d").strftime(lang[userLang]["FORMAT"])
+		info = [lang[userLang]["OPENED"].format(DATE = localize(self.dates[0], userLang))]
+		for d in self.dates[1 : -1 if self.isClosed else None]:
+			info.append(lang[userLang]["MOVED"].format(DATE = localize(d, userLang)))
+		if self.isClosed:
+			info.append(lang[userLang]["CLOSED"].format(DATE = localize(self.dates[-1], userLang)))
+		return "\n".join(info)
+
+	def telename(self, bold: bool = False, flag: bool = False, sid: bool = True) -> str:
+		c = ((flag, f"{self.flag} "), (bold, "*"), (True, f"Apple {self.name}"), (bold, "*"), (sid, f" ({self.rid})"))
+		return "".join(j for i, j in c if i)
 
 def StoreID(sid: int | str, fuzzy: Any = False, regular: Any = False) -> list[Store]:
 	try:
@@ -232,21 +211,23 @@ def reloadJSON(filename: str = DEFAULTFILE) -> str:
 	update = infoJSON.pop("update", None)
 	for sid, dct in infoJSON.items():
 		STORES[sid] = Store(sid = sid, dct = dct)
-	infoJSON["update"] = update
 	return update
 
 def sidify(sid: int | str, *, R: bool = False, fill: bool = True) -> str:
 	return f"{'R' if R and fill else ''}{str(sid).upper().removeprefix('R'):{'0>3' if fill else ''}}"
 
 def storeReturn(args: Optional[str | list[str]] = None, *, remove_closed: Any = False, remove_future: Any = False,
-	fuzzy: Any = False, regular: Any = False, split: Any = False, sort: Any = True) -> list[Store]:
-	args = args or "_"
+	fuzzy: Any = False, regular: Any = False, split: Any = False, sort: Any = True, filter: Optional[Callable] = None) -> list[Store]:
+	if not args or args == "_":
+		args, fuzzy = "_", True
 	if not isinstance(args, list):
 		args = re.split(r"\s*[,，]\s*", str(args)) if split else [str(args).strip()]
 	gen = set(g for s in args for m in (StoreID(s, fuzzy = fuzzy, regular = regular),
 		StoreMatch(s, fuzzy = fuzzy, regular = regular)) for g in m)
 	ans = [s for s in gen if (not remove_closed or not s.isClosed and not s.isIntern) and
 		(not remove_future or not s.isFuture and not s.isIntern)]
+	if filter:
+		ans = [i for i in ans if filter(i)]
 	if sort:
 		ans.sort()
 	return ans
