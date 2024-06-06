d773ce2bee30dda4ed7b9a78698bed8ba9783cb7
Junyi Lou <15688661+junyilou@users.noreply.github.com>
2024-06-06 15:20:03 +0800

    new: unified argparse

--- Today.py
+++ Today.py
@@ -1,15 +1,17 @@
 import asyncio
 import json
 import logging
+from argparse import ArgumentParser, Namespace
+from collections.abc import Sequence
 from datetime import datetime
-from sys import argv
-from typing import Optional
+from typing import Any, Optional
 
-from modules.today import Collection, Course, Schedule, Sitemap, Store, teleinfo
-from modules.util import get_session, setLogger, sortOD
+from modules.today import (Collection, Course, Schedule, Sitemap, Store,
+                           teleinfo)
+from modules.util import AsyncGather, get_session, setLogger, sortOD
 from storeInfo import storeReturn
 
-TODAYARGS = ["🇨🇳", "🇭🇰", "🇲🇴", "🇹🇼", "🇯🇵"]
+DEFAULT_FLAGS = "🇨🇳", "🇭🇰", "🇲🇴", "🇹🇼", "🇯🇵"
 
 async def post(text: str, image: str, keyboard: list[list[list[str]]]) -> Optional[dict]:
 	from bot import chat_ids
@@ -18,7 +20,7 @@ async def post(text: str, image: str, keyboard: list[list[list[str]]]) -> Option
 		"parse": "MARK", "chat_id": chat_ids[0], "keyboard": keyboard}
 	return await async_post(push)
 
-async def main(mode: str) -> None:
+async def entry(saved: dict[str, Any], mode: str, flags: Sequence[str]) -> None:
 	append = False
 	courses: dict[Course, set[Schedule]] = {}
 	results: list[Collection | Course | Schedule] = []
@@ -26,19 +28,19 @@ async def main(mode: str) -> None:
 	async with get_session() as session:
 		match mode:
 			case "today":
-				stores = storeReturn(TODAYARGS, opening = True)
+				stores = storeReturn(flags, opening = True)
 				tasks = [Store(store = store).getSchedules(session = session) for store in stores]
 			case "sitemap":
-				tasks = [Sitemap(flag = flag).getObjects(session = session) for flag in TODAYARGS]
+				tasks = [Sitemap(flag = flag).getObjects(session = session) for flag in flags]
 			case _:
 				return
-		runners = await asyncio.gather(*tasks, return_exceptions = True)
-	runners, exp = [[i for i in runners if b ^ isinstance(i, BaseException)] for b in [True, False]]
+		runners = await AsyncGather(tasks, return_exceptions = True)
+	runners, exp = [[i for i in runners if b ^ isinstance(i, Exception)] for b in (True, False)]
 	for e in exp:
-		assert isinstance(e, BaseException)
+		assert isinstance(e, Exception)
 		logging.error(repr(e))
 	r = {i for j in runners if isinstance(j, list) for i in j}
-	course_collection, schedule = [[i for i in r if b ^ isinstance(i, Schedule)] for b in [True, False]]
+	course_collection, schedule = [[i for i in r if b ^ isinstance(i, Schedule)] for b in (True, False)]
 	results = sorted(course_collection, key = lambda v: v.rootPath) + sorted(schedule)
 
 	for j in results:
@@ -86,7 +88,7 @@ async def main(mode: str) -> None:
 		if doSend:
 			logging.info(str(course))
 			schedules = [i for j in (courses[c] for c in courses if c.courseId == course.courseId) for i in j]
-			text, image, keyboard = teleinfo(course = course, schedules = schedules, prior = TODAYARGS)
+			text, image, keyboard = teleinfo(course = course, schedules = schedules, prior = flags)
 			await post(text, image, keyboard)
 
 	if append:
@@ -95,10 +97,19 @@ async def main(mode: str) -> None:
 		with open("Retail/savedEvent.json", "w") as w:
 			json.dump(sortOD(saved, reverse = [True, False]), w, ensure_ascii = False, indent = 2)
 
-setLogger(logging.INFO, __file__, base_name = True)
-logging.info("程序启动")
-argv[1:] = argv[1:] or ["today"]
-with open("Retail/savedEvent.json") as m:
-	saved = json.loads(m.read())
-asyncio.run(main(argv[1]))
-logging.info("程序结束")
+async def main(args: Namespace) -> None:
+	setLogger(logging.INFO, __file__, base_name = True, force_print = args.debug)
+	logging.info("程序启动")
+	with open("Retail/savedEvent.json") as r:
+		saved = json.load(r)
+	await entry(saved, "sitemap" if args.sitemap else "today", args.flags)
+	logging.info("程序结束")
+
+if __name__ == "__main__":
+	parser = ArgumentParser()
+	parser.add_argument("flags", metavar = "FLAG", type = str, nargs = "*", help = "指定国家或地区旗帜", default = DEFAULT_FLAGS)
+	parser.add_argument("-d", "--debug", action = "store_true", help = "打印调试信息")
+	parser.add_argument("-l", "--local", action = "store_true", help = "仅限本地运行")
+	parser.add_argument("-s", "--sitemap", action = "store_true", help = "网站地图模式")
+	args = parser.parse_args()
+	asyncio.run(main(args))
