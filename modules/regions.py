39cdad89a8bf26700e4908c7664326b0da60cc96
Junyi Lou <15688661+junyilou@users.noreply.github.com>
2024-06-20 23:19:42 +0800

    fix: sort regions keys

--- modules/regions.py
+++ modules/regions.py
@@ -6,9 +6,9 @@ from typing import Optional
 
 @dataclass(order = True, slots = True)
 class Region:
+	abbr: str
 	apu: bool
 	flag: str
-	abbr: str
 	locale: str
 	name: str
 	name_alt: list[str]
@@ -16,9 +16,9 @@ class Region:
 	part_sample: Optional[str]
 	post_location: str
 	url_app: Optional[str]
-	url_taa: str
 	url_retail: str
 	url_store: Optional[str]
+	url_taa: str
 
 	def __repr__(self) -> str:
 		return f"<{self.__class__.__name__} {self.abbr}: {self.name}>"
